b5d5a65e1e7acac1d21fb42f201e9808b9c84c20
Zhengjian liu <comezheng@gmail.com>
2023-10-28 21:44:08 -0400

    init

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View,Text,StyleSheet} from 'react-native';
+
+const SearchScreen = () =>{
+    return(
+        <Text>screen</Text>
+    )
+}
+
+export default SearchScreen;
